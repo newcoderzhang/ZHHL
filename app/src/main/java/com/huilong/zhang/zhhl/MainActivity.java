f484df0d7f9bee74e0911ec09163a94cd62b9e0c
zhangqian <zhangqiancompute@qq.com>
2016-03-26 16:17:51 +0800

    add some photes

--- app/src/main/java/com/huilong/zhang/zhhl/MainActivity.java
+++ app/src/main/java/com/huilong/zhang/zhhl/MainActivity.java
@@ -2,12 +2,27 @@ package com.huilong.zhang.zhhl;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.LinearLayout;
 
 public class MainActivity extends Activity {
+    private LinearLayout linearLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        linearLayout = (LinearLayout) findViewById(R.id.splish_root);
+        startAnim();
     }
+    private  void startAnim(){
+        RotateAnimation rotateAnimation = new RotateAnimation(0,360, Animation.RELATIVE_TO_SELF,0.5f,Animation.RELATIVE_TO_SELF,0.5f);
+        rotateAnimation.setDuration(1*1000);
+        rotateAnimation.setFillAfter(true);//保持动画后的状态
+
+        linearLayout.startAnimation(rotateAnimation);
+    }
+
 }
