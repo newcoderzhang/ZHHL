43ea0f9e38304f2dd021bb81ed525acc53e8f9ea
zhangqian <zhangqiancompute@qq.com>
2016-03-26 23:15:10 +0800

    Increase the initialization page and novice guide page

---
+++ app/src/main/java/com/huilong/zhang/zhhl/GuideActivity.java
@@ -0,0 +1,66 @@
+package com.huilong.zhang.zhhl;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+
+public class GuideActivity extends Activity {
+        private ViewPager viewPager;
+        private Button button;
+
+        private static  final  int[] mImageIds = new int[]{
+                R.mipmap.guide_1,
+                R.mipmap.guide_2,
+                R.mipmap.guide_3};
+    private ArrayList<ImageView> mImageViewList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guide);
+        init();
+        initData();
+    }
+    private void init(){
+        viewPager = (ViewPager) findViewById(R.id.vp_guade);
+    }
+    private void initData(){
+        mImageViewList = new ArrayList<ImageView>();
+        for (int i=0;i<mImageIds.length;i++){
+            ImageView imageView = new ImageView(this);
+            imageView.setBackgroundResource(mImageIds[i]);
+            mImageViewList.add(imageView);
+        }
+
+    }
+    class GuideAdapter extends PagerAdapter{
+
+        @Override
+        public int getCount() {
+            return mImageIds.length;
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return view ==object;
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            container.addView(mImageViewList.get(position));
+            return mImageViewList.get(position);
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            container.removeView((View) object);
+        }
+    }
+}
--- app/src/main/java/com/huilong/zhang/zhhl/MainActivity.java
+++ app/src/main/java/com/huilong/zhang/zhhl/MainActivity.java
@@ -2,8 +2,11 @@ package com.huilong.zhang.zhhl;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
 import android.view.animation.RotateAnimation;
+import android.view.animation.ScaleAnimation;
 import android.widget.LinearLayout;
 
 public class MainActivity extends Activity {
@@ -18,11 +21,27 @@ public class MainActivity extends Activity {
         startAnim();
     }
     private  void startAnim(){
+
+        AnimationSet set = new AnimationSet(false);
+        //旋转动画 
         RotateAnimation rotateAnimation = new RotateAnimation(0,360, Animation.RELATIVE_TO_SELF,0.5f,Animation.RELATIVE_TO_SELF,0.5f);
         rotateAnimation.setDuration(1*1000);
         rotateAnimation.setFillAfter(true);//保持动画后的状态
+        //缩放动画
+        ScaleAnimation scaleAnimation = new ScaleAnimation(0,1,0,1,Animation.RELATIVE_TO_SELF,0.5f,Animation.RELATIVE_TO_SELF,0.5f);
+        scaleAnimation.setDuration(1*1000);
+        scaleAnimation.setFillAfter(true);//保持动画后的状态
+        //缩放动画
+        AlphaAnimation alphaAnimation = new AlphaAnimation(0,1);
+        alphaAnimation.setDuration(1*1000);
+        alphaAnimation.setFillAfter(true);//保持动画后的状态
 
-        linearLayout.startAnimation(rotateAnimation);
+        set.addAnimation(rotateAnimation);
+        set.addAnimation(scaleAnimation);
+        set.addAnimation(alphaAnimation);
+
+
+        linearLayout.startAnimation(set);
     }
 
 }
