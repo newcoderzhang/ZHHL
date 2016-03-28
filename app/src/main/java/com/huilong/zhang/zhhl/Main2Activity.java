1c7752e2a1441d628cd5d231f87a7eb51800b614
zhangqian <zhangqiancompute@qq.com>
2016-03-28 22:58:03 +0800

    add slidingmenu success

--- app/src/main/java/com/huilong/zhang/zhhl/Main2Activity.java
+++ app/src/main/java/com/huilong/zhang/zhhl/Main2Activity.java
@@ -1,13 +1,39 @@
 package com.huilong.zhang.zhhl;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 
-public class Main2Activity extends AppCompatActivity {
+import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
+import com.jeremyfeinstein.slidingmenu.lib.app.SlidingFragmentActivity;
+
+public class Main2Activity extends SlidingFragmentActivity {
+    private static final String FRAGMENT_LEFT_MENU = "fragment_left_menu";
+    private static final String FRAGMENT_CONTENT = "fragment_content";
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+
+        setBehindContentView(R.layout.left_menu);// 设置侧边栏
+        SlidingMenu slidingMenu = getSlidingMenu();// 获取侧边栏对象
+        slidingMenu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);// 设置全屏触摸
+        slidingMenu.setBehindOffset(200);// 设置预留屏幕的宽度
+        //initFragment();
     }
+
+   /* private void initFragment() {
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction transaction = fm.beginTransaction();// 开启事务
+
+        transaction.replace(R.id.fl_left_menu, new LeftMenuFragment(),
+                FRAGMENT_LEFT_MENU);// 用fragment替换framelayout
+        transaction.replace(R.id.fl_content, new ContentFragment(),
+                FRAGMENT_CONTENT);
+
+        transaction.commit();// 提交事务
+        // Fragment leftMenuFragment = fm.findFragmentByTag(FRAGMENT_LEFT_MENU);
+    }*/
 }
