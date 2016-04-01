1745970d6fafee133be21555b77472a228747f6f
zhangqian <zhangqiancompute@qq.com>
2016-04-01 10:17:02 +0800

    add ragit button

--- app/src/main/java/com/huilong/zhang/zhhl/fragments/ContentFragment.java
+++ app/src/main/java/com/huilong/zhang/zhhl/fragments/ContentFragment.java
@@ -1,6 +1,7 @@
 package com.huilong.zhang.zhhl.fragments;
 
 import android.view.View;
+import android.widget.RadioGroup;
 
 import com.huilong.zhang.zhhl.R;
 
@@ -8,9 +9,17 @@ import com.huilong.zhang.zhhl.R;
  * Created by Mario on 3/29/16.
  */
 public class ContentFragment extends  BaseFragment {
+    private RadioGroup radioGroup;
     @Override
     public View initView() {
         View view = View.inflate(activity, R.layout.fragment_content, null);
+        radioGroup = (RadioGroup) view.findViewById(R.id.rg_group)；
         return view;
+    }
+
+    @Override
+    public void initData() {
+        radioGroup.check(R.id.rb_home);
+
     }
 }
